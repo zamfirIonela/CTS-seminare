97aa9094763fec53c545b4a9a3937dfa7fdb3cef
Zamfir Ionela <zamfirionela@Zamfirs-MacBook-Air-2.local>
2021-02-25 13:15:29 +0200

    commit 2

--- helloWorld/TestHello.java
+++ helloWorld/TestHello.java
@@ -1,7 +1,8 @@
 public class TestHello{
   
     public static void main(String[] args) {
-        System.out.println("\nHello\n");
+        System.out.println("\nHello");
+        System.out.println("\nCTS");
     }
     
 }
