c5be64108b19b61e43ec8286d863f651452e8655
zamfirionela <zamfirionela18@stud.ase.ro>
2021-04-01 12:43:05 +0300

    la pauza

--- Tema2/src/ro/ase/csie/cts/tema2/refactoring/Account.java
+++
@@ -1,6 +0,0 @@
-package ro.ase.csie.cts.tema2.refactoring;
-
-public enum Account {
-	STANDARD, BUGET, PREMIUM, SUPER_PREMIUM
-
-}
--- Tema2/src/ro/ase/csie/cts/tema2/refactoring/RataLunaraInterfata.java
+++
@@ -1,6 +0,0 @@
-package ro.ase.csie.cts.tema2.refactoring;
-
-public interface RataLunaraInterfata {
-	public double getRataLunara(double valoareImprumut, double valoareRata);
-
-}
