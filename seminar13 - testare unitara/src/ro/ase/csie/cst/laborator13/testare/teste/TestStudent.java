c4c233c5d5337530da9597d8883e91d105ce8f1c
zamfirionela <zamfirionela18@stud.ase.ro>
2021-05-27 13:26:50 +0300

    implementare teste

--- seminar13 - testare unitara/src/ro/ase/csie/cst/laborator13/testare/teste/TestStudent.java
+++ seminar13 - testare unitara/src/ro/ase/csie/cst/laborator13/testare/teste/TestStudent.java
@@ -11,6 +11,7 @@ import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import ro.ase.csie.cst.laborator13.testare.exceptii.ExceptieNota;
 import ro.ase.csie.cst.laborator13.testare.exceptii.ExceptieNume;
 import ro.ase.csie.cst.laborator13.testare.exceptii.ExceptieVarsta;
 import ro.ase.csie.cst.laborator13.testare.modele.Student;
@@ -92,6 +93,50 @@ public class TestStudent {
 	public void testSetNumeErrorCondition() throws ExceptieNume {
 		String numeNou = "Io";
 		student.setNume(numeNou);
+	}
+	
+	
+	@Test
+	public void testGetNotaMinimaOrderingSetSortatCrescator() throws ExceptieNota {
+		int notaMinima = 4;
+		ArrayList<Integer> note = new ArrayList<>();
+		for(int i=0; i<5; i++) {
+			note.add(i+notaMinima);
+		}
+		student.setNote(note);
+		
+		int notaMinimaCalculata = student.getNotaMinima();
+		assertEquals("Test cu valori sortate crescator", notaMinima, notaMinimaCalculata);
+		
+	}
+	
+	@Test
+	public void testGetNotaMinimaCardinalityZero() throws ExceptieNota {
+		ArrayList<Integer> note = new ArrayList<>();
+		student.setNote(note);
+		int notaMinima=0;
+		int notaMinimaCalculata = student.getNotaMinima();
+		assertEquals("Test fara note", notaMinima,notaMinimaCalculata);
+	
+	}
+	
+	@Test
+	public void testGetNotaMinimaCardinalityUnu() throws ExceptieNota {
+		ArrayList<Integer> note = new ArrayList<>();
+		note.add(Student.MAX_NOTA);
+		student.setNote(note);
+		int notaMinima=student.MAX_NOTA;
+		int notaMinimaCalculata = student.getNotaMinima();
+		assertEquals("Test fara note", notaMinima,notaMinimaCalculata);
+	
+	}
+	
+	@Test
+	public void testGetNotaMinimaExistanceReferintaNoteNull() throws ExceptieNota {
+		student.setNote(null);
+		int notaMinima = 0;
+		int notaMinimaCalculata = student.getNotaMinima();
+		assertEquals("Test cu referinta null pentru note", notaMinima,notaMinimaCalculata);
 	}
 
 }
